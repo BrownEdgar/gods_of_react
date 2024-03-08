dd824750769588609e3f720d1444b9b94f7c092f
arsenkoshatashyan <arsen.koshatashyan@mail.ru>
2024-03-08 13:43:23 +0400

    HM1

--- src/Homework/HW1/App.jsx
+++ src/Homework/HW1/App.jsx
@@ -1,36 +1,34 @@
 import React, { useEffect, useState } from "react";
-import './App.css'
+import "./App.css";
 
 export default function App() {
-  const [todos, setTodos] = useState([])
-  
-  
+  const [todos, setTodos] = useState([]);
+
+  const deleteByIndex = (index) => {
+    setTodos(todos.toSpliced(index, 1));
+  };
+ 
 
   useEffect(() => {
-
     fetch(`https://jsonplaceholder.typicode.com/todos`)
-      .then(res => res.json())
-      .then(data => setTodos(data));
+      .then((res) => res.json())
+      .then((data) => setTodos(data));
   }, []);
-
-
 
   return (
     <div className="App">
-        {todos.map(elem => {
-          return(
-            <div className="Todos" key={elem.id}>
-             <div className="Todos__button">
-             <button >&#10004;</button>
-             <button >		&#10006;</button>
-             </div>
-              <h3>{elem.id}</h3>
-              <p>{elem.title}</p>
+      {todos.map((elem, index) => {
+        return (
+          <div className="Todos" key={elem.id}>
+            <div className="Todos__button">
+              <button >&#10004;</button>
+              <button onClick={() => deleteByIndex(index)}>&#10006;</button>
             </div>
-          )
-        })} 
+            <h3>{elem.id}</h3>
+            <p>{elem.title}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
-
-
