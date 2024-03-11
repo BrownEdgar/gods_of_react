543613441bd895f5ad609cfe2893c199b9968b81
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-03-11 20:30:34 +0400

    fix homework

--- src/Homeworks/Movies/App.jsx
+++ src/Homeworks/Movies/App.jsx
@@ -4,41 +4,36 @@ import data from './data.json';
 
 
 export default function App() {
-    const[movies, setMovies] = useState([])
+  const [movies, setMovies] = useState(data)
 
-    useEffect(() => {
-        setMovies(data);
-      }, []);
 
-    const deleteItem = (index) => {
-        //setMovies(movies.toSpliced(index, 1))
-        const updatedMovies = [...movies];
-        updatedMovies.splice(index, 1);
-        setMovies(updatedMovies);
-    };
-      
+  const deleteItem = (index) => {
+    console.log('index', index)
+    setMovies(movies.toSpliced(index, 1));
+  };
 
-    return (
-        <div className='App'>
-          <h1>Movie List</h1>
-          <div className='App__container'>
-            <div className="App__hearder App__container__row">
-                <p>Title</p>
-                <p>Genre</p>
-                <p>Stock</p>
-                <p>Rate</p>
-                <p></p>
-            </div>
-            {data.map((movie, index) => (
-              <div className='App__container__row' key={movie.id}>
-                <p>{movie.title}</p>
-                <p>{movie.genre}</p>
-                <p>{movie.stock}</p>
-                <p>{movie.rate}</p>
-                <button onClick={() => deleteItem(index)}>Delete</button>
-              </div>
-            ))}
-          </div>
+
+  return (
+    <div className='App'>
+      <h1>Movie List</h1>
+      <div className='App__container'>
+        <div className="App__hearder App__container__row">
+          <p>Title</p>
+          <p>Genre</p>
+          <p>Stock</p>
+          <p>Rate</p>
+          <p></p>
         </div>
-      );
+        {movies.map((movie, index) => (
+          <div className='App__container__row' key={movie.id}>
+            <p>{movie.title}</p>
+            <p>{movie.genre}</p>
+            <p>{movie.stock}</p>
+            <p>{movie.rate}</p>
+            <button onClick={() => deleteItem(index)}>Delete</button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
