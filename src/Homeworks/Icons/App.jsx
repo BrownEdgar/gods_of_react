62f6d426349b579ef1e3a1a36972e83edd1ff288
RafaellaMadantsyan <rafaella.madantsyan1@gmail.com>
2024-03-13 15:42:35 +0400

    + icons

---
+++ src/Homeworks/Icons/App.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import 'boxicons'
+import './App.scss'
+
+
+export default function App() {
+  return (
+    <div className='App'>
+      <div className="App__btns">
+        <div className="btn">
+            <i class='bx bxl-facebook'></i>
+        </div>
+        <div className="btn">
+            <i class='bx bxl-twitter' ></i>
+        </div>
+        <div className="btn">
+            <i class='bx bxl-google' ></i>
+        </div>
+        <div className="btn">
+            <i class='bx bxl-instagram' ></i>
+        </div>
+        <div className="btn">
+            <i class='bx bxl-youtube' ></i>
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Homeworks/CreateAccount/App.jsx'
+import App from './Homeworks/Icons/App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
