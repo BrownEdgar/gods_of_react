a5f4b2f2f3b4385015cbd7cf01bf225522492a8f
ani1990ani <anahit.darbinian@gmail.com>
2024-03-18 11:26:31 +0400

    Modal2

--- src/Lessons/ModalExample/App.jsx
+++ src/Lessons/ModalExample/App.jsx
@@ -6,19 +6,37 @@ import Modal from '../../components/Modal/Modal';
 export default function App() {
     const [data, setData] = useState(
         ['html', 'css', 'Java Script', 'React.js', 'Node.js', 'Python']
-    )
+    );
+    const [isOpen, setIsOpen] = useState(false);
+    const [currentItem, setCurrentItem] = useState('')
+    const toogleModal=()=> setIsOpen(!isOpen)
+    const deleteItemByName=()=>{
+      const result = data.filter(elem => elem !== currentItem);
+      setData(result);
+      setCurrentItem('');
+      toogleModal();
+    }
+
   return (
     <div className="App">
-      <Modal/>
-      <h1 className="App__title">React Modal example</h1>
-        {data.map(elem=>{
-          return(
-            <div key={elem} className="App__item">
-              <h2>{elem}</h2>
-              <button>Delete</button>
-            </div>
-          )
-        })}
+     <Modal toogleModal={toogleModal} isOpen={isOpen}>
+      <h1>Are you sure?</h1>
+      <button onClick={toogleModal}>close</button>
+      <button onClick={deleteItemByName}>delete</button>
+
+     </Modal>
+     <h1 className='App__title'>React Modal example</h1>
+     {data.map(elem=>{
+      return(
+        <div key={elem} className='App__item'>
+          <h2>{elem}</h2>
+          <button onClick={()=>{
+            toogleModal()
+            setCurrentItem(elem)
+          }}>delete</button>
+        </div>
+      )
+     })}
     </div>
   )
 }
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
 import './Modal.scss'
 
-export default function Modal() {
+
+
+export default function Modal({children, theme, toogleModal, isOpen}) {
+  useEffect(() => {
+    const handleClick=(e)=>{
+      const {className} = e.target;
+      if(className === 'Modal'){
+        toogleModal()
+      }
+      console.log('click')
+
+    }
+    window.addEventListener('click', handleClick)
+    
+  
+    return () => {
+      window.removeEventListener('click', handleClick)
+    }
+  }, [ ])
+  if(!isOpen){
+    return null;
+  }
+  
   return (
     <div className="Modal">
-        <div className="Modal__content">
-            <h1>Modal</h1>
+        <div className={classNames('Modal__content', {[`Modal__content-${theme}`] : true})}>
+            {children}
         </div>
     </div>
   )
+}
+
+
+
+Modal.defaultProps={
+  theme: 'light'
+}
+
+Modal.propTypes={
+isOpen: PropTypes.bool.isRequired,
+toogleModal: PropTypes.func.isRequired,
+theme: PropTypes.oneOf(['dark', 'light'])
+//children: PropTypes.oneOfType([
+//  PropTypes.element,
+//  PropTypes.arrayOf(PropTypes.node)
+//])
+
 }
