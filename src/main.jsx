fba29a744707a3fa5e168a9cf44b39616c54d6b2
Levon <levon.marutyan1998@gmail.com>
2024-03-22 00:35:30 +0400

    FORM HW

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/Lessons/Form/App'
+import App from './components/HOMEWORKS/HW-FORM/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
