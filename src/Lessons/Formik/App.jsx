31950e60310cb791844bc8ec5c1c430e18c18874
ani1990ani <anahit.darbinian@gmail.com>
2024-03-24 13:29:46 +0400

    formik verjnakan

--- src/Lessons/Formik/App.jsx
+++ src/Lessons/Formik/App.jsx
@@ -1,48 +1,78 @@
-
+import React, { useState } from 'react'
 import * as yup from 'yup'
-import {Formik, Form, Field, ErrorMessage} from 'formik'
-import { nanoid } from 'nanoid'
-import React, { useState} from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 import './App.scss'
+import { nanoid } from 'nanoid'
 
+
+const initialValues = {
+  username: '',
+  email: '',
+  gender: '',
+  language: []
+}
 const validationSchema = yup.object({
-  username: yup.string().max(20, "petq e lini max 20").min(3, "3 simvolic voch pakas").required(),
+  username: yup.string().min(3, '3 simvolic voch pakas').max(20, "petq e lini maximum 20").required(),
   email: yup.string().email().required(),
-  gender: yup.string().oneOf(['male', 'female'])
+  gender: yup.string().oneOf(['male', 'female']),
+  language: yup.array().oneOf(['js', 'react', 'next', 'node']).min(2)
 })
 
 export default function App() {
   const [users, setUsers] = useState([])
-  const handleSumbit = (values) =>{
+
+  const handleSubmit = (values) => {
     console.log(values)
-    const user ={
+    const user = {
       id: nanoid(4),
       ...values,
     }
     setUsers([...users, user])
   }
   return (
-    <div className='App'>
+    <div className='App' >
       <h1>Formik example</h1>
-      <Formik 
-      initialValues={{username: '', email: '', gender: 'male'}}
-      onSubmit={handleSumbit}
-      validationSchema={validationSchema}
-      validateOnChange={true}
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+        validateOnChange
       >
         <Form>
-          <Field type='email' name='email'/>
-          <ErrorMessage name='email' component='p' className="error"/>
-          <Field type='text' name='username'/>
-          <ErrorMessage name='username' component='p' className="error"/>
-          <Field as='select' name="gender">
-            <option value="male">male</option>
-            <option value="female">female</option>
-          </Field>
-          <ErrorMessage name='gender'component='p' className="error"/>
+          <Field type='email' name='email' />
+          <ErrorMessage name='email' component='p' className='error' />
+          <Field type='text' name='username' />
+
+          <ErrorMessage name='username' component='p' className='error' />
+          {/* <ErrorMessage name='username'>
+            {
+              (errorMessage) => {
+                return (
+                  <div>
+                    <p className="error">
+                      <span>{errorMessage}</span>
+                    </p>
+                  </div>
+                )
+              }
+            }
+          </ErrorMessage> */}
+
+
+
+
+          <Field type='radio' name='gender' value='male' />male
+          <Field type='radio' name='gender' value='female' />female
+          <ErrorMessage name='gender' component='p' className='error' />
+          <Field type='checkbox' name='language' value='js' /> js
+          <Field type='checkbox' name='language' value='react' /> react
+          <Field type='checkbox' name='language' value='next' /> next
+          <Field type='checkbox' name='language' value='node' /> node
+
+          <ErrorMessage name='language' component='p' className='error' />
           <input type="submit" />
         </Form>
       </Formik>
-    </div>
+    </div >
   )
 }
