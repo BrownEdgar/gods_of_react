469e7a300b631d2aff630cea86b45aba3616fc23
Levon <levon.marutyan1998@gmail.com>
2024-03-24 23:45:34 +0400

    Formik_1

---
+++ src/components/HOMEWORKS/HW-FORMIK/App.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import * as yup from 'yup'
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import './App.scss'
+import { nanoid } from 'nanoid';
+
+const initialValues = {
+  username: '',
+  email: '',
+  gender: '',
+  color: '',
+  language: []
+}
+
+const validationScheme = yup.object({
+  username: yup.string().min(3, '3 sivolic voch pakas').max(20, '20 simvolic qich').required(),
+  email: yup.string().email().required(),
+  gender: yup.string().oneOf(['male', 'female']).required(),
+  language: yup.array().min(2).required(),
+})
+
+export default function App() {
+
+  const [users, setUsers] = useState([])
+
+  const handleSubmit = (values) => {
+    console.log(values);
+    const user = {
+      id: nanoid(4),
+      ...values,
+    }
+    setUsers([...users, user])
+  }
+  return (
+    <div className='App'>
+      <h1>Formik</h1>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationScheme={validationScheme}
+        validateOnChange
+      >
+        <Form>
+          <Field type='text' name='username' />
+          <ErrorMessage name='username' component='p' className="error" />
+          <Field type='email' name='email' />
+          <ErrorMessage name='email' component='p' className="error" />
+          <Field type='radio' name='gender' value='male' />male
+          <Field type='radio' name='gender' value='female' />female
+          <ErrorMessage name='gender' component='p' className="error" />
+          <Field type='checkbox' name='language' value='js' />js
+          <Field type='checkbox' name='language' value='react' />react
+          <Field type='checkbox' name='language' value='next' />next
+          <Field type='checkbox' name='language' value='node' />node
+          <ErrorMessage name='language' component='p' className="error" />
+          <input type="submit" />
+        </Form>
+      </Formik>
+    </div>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/HOMEWORKS/HW-FORM/App'
+import App from './components/HOMEWORKS/HW-FORMIK/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
