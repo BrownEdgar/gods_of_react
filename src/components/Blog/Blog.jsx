5e5efc96567b9f20db80d161f6aee05f2245b065
ani1990ani <anahit.darbinian@gmail.com>
2024-03-28 23:59:49 +0400

    Pagination start

---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Blog.scss'
+
+export default function Blog({ blog }) {
+  return (
+    <div className='Blog'>
+      <img src={blog.poster} alt="" />
+      <div className="Blog__content">
+        <h2>{blog.title}</h2>
+        <p>{blog.desc}</p>
+        <div className="Blog__footer">
+          <img src={blog.avatar} className='Blog__avatar' />
+          <p>
+            {blog.date}
+          </p>
+          <a href="!#">Read post</a>
+        </div>
+      </div>
+    </div>
+  )
+}
