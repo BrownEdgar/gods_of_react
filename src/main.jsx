56506970bcaca958512835394be2cfa2a4bd9ef3
Levon <levon.marutyan1998@gmail.com>
2024-03-28 21:26:47 +0400

    PAGINATION

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/Lessons/useReduser/App'
+import App from './components/Lessons/BlogPagination/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
