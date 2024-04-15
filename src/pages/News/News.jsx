55339ce20e15b7b8564a354c53fc11b15afdd74f
ani1990ani <anahit.darbinian@gmail.com>
2024-04-15 10:06:28 +0400

    json

---
+++ src/pages/News/News.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import './News.scss'
+
+
+export default function News() {
+    const [state, setState] = useState([])
+    useEffect(() => {
+     
+    }, [])
+    
+  return (
+    <div>News</div>
+  )
+}
