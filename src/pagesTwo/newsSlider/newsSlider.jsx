f4d073e1e02984298662a6db7bd6e5b7c8ca0e87
arsenkoshatashyan <arsen.koshatashyan@mail.ru>
2024-04-16 15:34:46 +0400

    slickSlider

--- src/pagesTwo/newsSlider/newsSlider.jsx
+++ src/pagesTwo/newsSlider/newsSlider.jsx
@@ -1,47 +1,36 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Slider from "react-slick";
 
-
-import "./newsSlider.scss";
-import moment from "moment";
-import reducer from "./reducer";
-
-
-
+import './newsSlider.scss'
 
 export default function newsSlider() {
-  const [state, dispatch] = useReducer(reducer, []);
+  const [slider, setSlider] = useState([]);
 
-
-    const settings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-    };
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+  };
 
   useEffect(() => {
-    axios(import.meta.env.VITE_DB_URL).then((res) =>
-      dispatch({ type: "slider", payload: res.data })
-    );
+    axios(import.meta.env.VITE_DB_URL).then((res) => setSlider(res.data));
   }, []);
-
-
 
   return (
     <div className="newsSlider">
-       <Slider {...settings}>
-       {state.map((elem) => {
-        return (
-          <div className="newsSlider__content" key={elem.id}>
+      <Slider {...settings}>
+        {slider.map((elem) => {
+          return (
+            <div className="newsSlider__content" key={elem.id}>
               <img src={elem.image} />
-            <p>{elem.title}</p>
-          </div>
-        );
-      })}
-       </Slider>
+              <p>{elem.title}</p>
+            </div>
+          );
+        })}
+      </Slider>
     </div>
   );
-  }
+}
