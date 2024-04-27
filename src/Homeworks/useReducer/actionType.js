369ece2ee6d9cb5fd5cdc2b7f0b21a6d596e70f2
arsenkoshatashyan <arsen.koshatashyan@mail.ru>
2024-04-27 12:22:59 +0400

    removed

--- src/Homeworks/useReducer/actionType.js
+++
@@ -1,7 +0,0 @@
-export const CHANGE_USER = 'CHANGE_USER'
-export const ADD_RANDOM = 'ADD_RANDOM'
-export const SHUFFLE = 'SHUFFLE'
-export const TEN = "TEN"
-export const ADD_USERNAME = 'ADD_USERNAME'
-
-
