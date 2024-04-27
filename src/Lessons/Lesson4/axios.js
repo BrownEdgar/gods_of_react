877fb05a7193e4beaa5392ca870cef019d474b23
arsenkoshatashyan <arsen.koshatashyan@mail.ru>
2024-04-27 12:19:43 +0400

    lessons

--- src/Lessons/Lesson4/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const instance = axios.create({
-    baseURL: import.meta.env.VITE_DB_URL,
-    timeout: 3000,
-    
-})
-
-export default instance
--- src/Lessons/useReducer/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const ADD_ITEM = 'ADD_ITEM'
-export const SET_TEN = 'SET_TEN'
-export const SORT = 'SORT'
-export const DELETE_BY_INDEX = 'DELETE_BY_INDEX'
