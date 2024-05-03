959b5ecb117a92454a0560d097b54d90ae7d7e14
arsenkoshatashyan <arsen.koshatashyan@mail.ru>
2024-05-03 13:59:37 +0400

    redux

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,22 +1,44 @@
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import axios from "axios";
+
+// export const getPosts = createAsyncThunk("posts/getPosts", async (postId) => {
+//   const response = await axios(
+//     `https://jsonplaceholder.typicode.com/posts/${postId}`
+//   );
+//   return response.data;
+// });
+
+// const postsSlice = createSlice({
+//   name: "posts",
+//   initialState: [],
+//   reducers: {},
+//   extraReducers: (builder) => {
+//     builder.addCase(getPosts.fulfilled, (state, action) => {
+//       return state.push(action.payload);
+//     });
+//   },
+// });
+
+// export default postsSlice.reducer;
+
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
 export const getPosts = createAsyncThunk("posts/getPosts", async (postId) => {
   const response = await axios(
-    `https://jsonplaceholder.typicode.com/posts${postId}`
+    `https://jsonplaceholder.typicode.com/posts/${postId}`
   );
   return response.data;
 });
 
 const postsSlice = createSlice({
-  name: "posts",
+  name: "counter",
   initialState: [],
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(getPosts.fulfilled, (state, action) => {
-      return state.push(action.payload);
+       state.push(action.payload);
     });
   },
 });
-
 export default postsSlice.reducer;
